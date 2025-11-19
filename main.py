70f86ce8e9477f9780abd8ac60e0568e14039bfd
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2025-11-19 15:36:26 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+#11-misol
+s = "GrokByxAi"
+print(s)
+
+toza = s[:4], s[-3:]
+print(toza)
+
+
+#12-misol
+s = "9876543210"
+print(s)
+
+toza = s[::2]
+toza2 = toza[::-1]
+print(toza2)
+
+
+#13-misol
+s = "MachineLearning"
+print(s)
+
+toza = s[7:12]
+print(toza)
+toza2 = toza[1:4]
+print(toza2)
+
+
+#14-misol
+s = "PythonJavaJavaScript"
+print(s)
+
+soni = s.count("Java")
+print(f"Java lar soni: {soni}")
+
+res = "Java" * soni
+print(res)
+
+
+#15-misol
+s = "abcdefghijk"
+print(s)
+
+toza = s[:5] + s[-5:]
+print(toza)
+
+
+#16 - misol
+s = "OpenAIxAIGoogleAI"
+print(s)
+
+soni = s.count("AI")
+print(f"Ai lar soni: {soni}")
+
+res = "AI" * soni
+print(res)
